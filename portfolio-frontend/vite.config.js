f0685d16e962048c36eb368ded126b6f469c9300
Med El Mobarik <mohamed.elmobarik@apitech.fr>
2025-03-23 17:38:45 +0100

    feat: migrate to vite
    BREAKING CHANGE: migrate to vite and vitest

---
+++ portfolio-frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+  },
+  server: {
+    host: '0.0.0.0',
+  },
+})
