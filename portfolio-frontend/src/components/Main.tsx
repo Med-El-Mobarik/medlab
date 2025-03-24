6fadf37fbc415c7f765ff4be0d37b9530cf0ce32
Med El Mobarik <mohamed.elmobarik@apitech.fr>
2025-03-24 01:53:57 +0100

    Revert "photo and title (#15)"
    This reverts commit e30146a6196a6784e5da4f02c10db9fc798567c4.

--- portfolio-frontend/src/components/Main.tsx
+++ portfolio-frontend/src/components/Main.tsx
@@ -12,7 +12,7 @@ function Main() {
       <div className="about-section">
         <div className="image-wrapper">
           <img
-            src="/images/profile.jpg"
+            src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg"
             alt="Avatar"
           />
         </div>
