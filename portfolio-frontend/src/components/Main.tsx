de44c6405a5d21d8a0058d679622491f3da3bc34
Med El Mobarik <mohamed.elmobarik@apitech.fr>
2025-04-14 23:37:51 +0200

    feat: Add phone and email, get cv

--- portfolio-frontend/src/components/Main.tsx
+++ portfolio-frontend/src/components/Main.tsx
@@ -3,6 +3,10 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import "../assets/styles/Main.scss";
 import { useTranslation } from "react-i18next";
+import Button from "@mui/material/Button";
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import EmailIcon from "@mui/icons-material/Email";
 
 function Main() {
   const { t } = useTranslation();
@@ -30,8 +34,26 @@ function Main() {
               <LinkedInIcon />
             </a>
           </div>
+          <div className="contact_infos">
+            <a href="tel:+33774375682">
+              <LocalPhoneIcon />
+              <p>+33 7 74 37 56 82</p>
+            </a>
+            <a href="mailto:mo.elmobarik@gmail.com">
+              <EmailIcon />
+              <p>mo.elmobarik@gmail.com</p>
+            </a>
+          </div>
           <h1>Mohamed El Mobarik</h1>
           <p>{t("job")}</p>
+
+          <Button
+            variant="contained"
+            endIcon={<ArrowOutwardIcon />}
+            onClick={() => window.open("/cv.pdf", "_blank")}
+          >
+            {t("resume")}
+          </Button>
 
           <div className="mobile_social_icons">
             <a
